7908d899e5db140e28cf8d13ba9f09cf431a1e6f
kristiansen616 <52787766+kristiansen616@users.noreply.github.com>
2020-09-15 22:56:39 -0300

    git ignore

---
+++ dist/spa/js/4.75ba787b.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[4],{"54d6":function(t,a,n){"use strict";n.r(a);var s=function(){var t=this,a=t.$createElement,n=t._self._c||a;return n("q-page",{staticClass:"container"},[n("div",{staticClass:"row"},[n("h1",[t._v("DashBoard")])])])},e=[],c=n("2877"),l={},r=Object(c["a"])(l,s,e,!1,null,null,null);a["default"]=r.exports}}]);
