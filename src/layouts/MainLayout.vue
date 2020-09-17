adeed1fa8aec5359bd394632284f565a43322885
kristiansen616 <52787766+kristiansen616@users.noreply.github.com>
2020-09-17 17:35:49 -0300

    espaço

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -19,6 +19,7 @@
       @input="dark()"
     />
     <q-icon name="fas fa-sun" />
+    
        <q-btn flat style="color: #ffffff" label="Alertas" @click="alert = true" />
    <q-dialog v-model="alert">
       <q-card>
