d8e314352cd6c74a94d81d9656f45884ae4ac793
kristiansen616 <52787766+kristiansen616@users.noreply.github.com>
2020-09-17 17:48:15 -0300

    alteração do texto do notify

--- src/pages/dashboard/dashboard.vue
+++ src/pages/dashboard/dashboard.vue
@@ -138,14 +138,14 @@ export default {
           color: 'red-5',
           textColor: 'white',
           icon: 'warning',
-          message: 'You need to accept the license and terms first'
+          message: 'você precisa aceitar a licença e termos de primeira'
         })
       } else {
         this.$q.notify({
           color: 'green-4',
           textColor: 'white',
           icon: 'cloud_done',
-          message: 'Submitted'
+          message: 'submetido'
         })
       }
     },
